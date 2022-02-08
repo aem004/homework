da702277d9e33bd213a2d202d6424fe3cfa88fe5
aem004 <81263058+aem004@users.noreply.github.com>
2022-02-08 19:43:57 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let city = 'Москва';
+let country = 'Россия';
+let population = 11920000;
+let stadium = true;
+console.log(city);
+console.log(country);
+console.log(population);
+console.log(stadium);

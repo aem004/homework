1ea1305e623a8e3be8212b1409a47f654e4c40a0
aem004 <81263058+aem004@users.noreply.github.com>
2022-02-08 20:31:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 /* 
     Задание 1:
-
-    Вам необходимо поделиться информацией о вашем родном городе. Все данные необходимо записать в отдельную переменную.
-    Информация о городе: 
-    - Название города (строка)
-    - В какой стране находится этот город (строка)
-    - Численность населения (число)
-    - Есть ли футбольный стадион (boolean [ true(да) / false(нет) ])
     
 let city = 'Москва';
 let country = 'Россия';
@@ -22,13 +15,6 @@ console.log(stadium);
 /* 
     Задание 2:
     
-    Напишите скрипт, который находит площадь прямоугольника
-
-    - высота 40см
-    - ширина 70см
-
-    ps: каждая сущность должна находиться в отдельной переменной
-    
 let S;
 var a = prompt("Введите a: ");
 var b = prompt("Введите b: ");
@@ -36,28 +22,30 @@ var b = prompt("Введите b: ");
 S = a * b;
 console.log(S);
 
-/* или
+или 
+
 let a = 40;
 let b = 70;
 let S;
 S = a * b;
 console.log(S);
- */
+
 */
 
 /* 
     Задание 3:
     
-    Два автомобиля одновременно выехали навстречу друг другу из двух городов и встретились через 2 часа.
-    Первый ехал со скоростью 95км/ч, а второй 114км/ч.
+const time = 2;
+const speedOfFirst = 95;
+const speedOfSecond = 114;
 
-    Цель: Выяснить на каком расстоянии находятся города друг от друга и после всех вычеслений записать результат в переменную.
+let approachspeed;
+let distance;
 
-    Исходные данные: 
-    time = 2;
-    speedOfFirst = 95;
-    speedOfSecond = 114;
-    
+approachspeed = speedOfFirst + speedOfSecond;
+distance = approachspeed * time;
+
+console.log(distance);
     
 
 */
@@ -65,21 +53,37 @@ console.log(S);
 /* 
     Задание 4:
     
-    Перед вами код:
-    const randomNumber = Math.floor(Math.random() * 100);
+const randomNumber = Math.floor(Math.random() * 100);
 
-    Этот код при каждом обновлении страницы генерирует случайное число и записывает его в переменную randomNumber.
+console.log(randomNumber);
 
-    Напишите условную конструкцию, со следующими данными:
-    - если randomNumber меньше 20, то выведите в консоль сообщение : "randomNumber меньше 20"
-    - если randomNumber больше 50, то выведите в консоль сообщение : "randomNumber больше 50"
-    - если ни один из вариантов не совпал, то выведите в консоль сообщение : "randomNumber больше 20, и меньше 50"
+if (randomNumber < 20) {
+    console.log('randomNumber меньше 20');
+} else if (randomNumber > 50) {
+    console.log('randomNumber больше 50');
+} else {
+    console.log('randomNumber больше 20, и меньше 50');
+}
+
 */
 
 /* 
     Задание 5:
-    
-    Условную конструкцию из задания 4, перепишите с помощью Switch Case
+const randomNumber = Math.floor(Math.random() * 100);
+
+console.log(randomNumber);
+
+switch(true){
+    case (randomNumber < 20) :
+        console.log('randomNumber меньше 20');
+        break;
+    case (randomNumber > 50) :
+        console.log('randomNumber больше 50');
+        break;
+    default :
+        console.log('randomNumber больше 20, и меньше 50');
+}
+
 */
 
 
